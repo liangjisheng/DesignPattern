35399774925abbb3af3486842161a0ee7ee823e7
Jisheng Liang <1294851990@qq.com>
2018-03-13 16:24:20 +0800

    Add files via upload

---
+++ FactoryPattern/SimpleFactory/client.cpp
@@ -0,0 +1,20 @@
+
+#include "SimpleFactory.h"
+
+int main()
+{
+	AbstractFactory* factory = new SimpleFactory();
+
+	AbstractProduct* product = factory->createProduct(1);
+	product->operation();
+	delete product;
+	product = NULL;
+
+	product = factory->createProduct(2);
+	product->operation();
+	delete product;
+	product = NULL;
+
+	getchar();
+	return 0;
+}
